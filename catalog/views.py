6e68678f501769fc9c4f5f10c853d2de35974160
albina_kazartseva <albinak74@gmail.com>
2025-05-02 16:37:29 +0500

    Homework_22 done

---
+++ catalog/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+
+# def contacts(request):
+#     return render(request, 'contacts.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name)
+        print(message)
+        print(phone)
+        # Здесь мы просто возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'contacts.html')
